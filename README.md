a8464e1bb451cd4090f03a23682556fafce6799b
huongly9 <huongly9119@gmail.com>
2021-10-21 18:24:44 +0700

    Change files to have a neater look

--- README.md
+++ README.md
@@ -1,12 +1,2 @@
 # The Number Guessing Game
  A fun number guessing game to help you get better chance at gambling
-
-code adapted from https://teamtreehouse.com/community/how-to-check-if-input-is-int-and-not-string-or-float
-by Joey Bruijns
-try:
-     number = input("Please enter a number: ")
-     int(number)
-except ValueError:
-     print("That's not a number..")
-else:
-     print(number)
--- guessing_game.py
+++ guessing_game.py
@@ -1,31 +1,3 @@
-"""
-Python Web Development Techdegree
-Project 1 - Number Guessing Game
---------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
-"""
-
-"""Psuedo-code Hints
-
-When the program starts, we want to:
-------------------------------------
-1. Display an intro/welcome message to the player.
-2. Store a random number as the answer/solution.
-3. Continuously prompt the player for a guess.
-  a. If the guess greater than the solution, display to the player "It's lower".
-  b. If the guess is less than the solution, display to the player "It's higher".
-
-4. Once the guess is correct, stop looping, inform the user they "Got it"
-     and show how many attempts it took them to get the correct number.
-5. Let the player know the game is ending, or something that indicates the game is over.
-
-( You can add more features/enhancements if you'd like to. )
-"""
-# write your code inside this function.
 
 def start_game():
     while True:
@@ -34,7 +6,7 @@ def start_game():
             int(guessing_question)
         except ValueError:
             print("Teaser! That's not a valid value. Try again!")
-            continue   
+            continue
         if int(guessing_question) < 0 or int(guessing_question) > 10:
             print("Oops!... We did it again! The number must be from 0 to 10.")
             continue
@@ -42,29 +14,28 @@ def start_game():
             guessing_number = int(guessing_question)
             return guessing_number
 
-# Kick off the program by calling the start_game function.
 
 print("*** WELCOME TO THE GUESSING GAME ***")
 import random
 number = random.randint(0,10)
 attempt = 0
 score_list = []
-    
+
 while True:
-    
+
     x = start_game()
 
     if x > number:
         print("Oh no! It's lower. Please try again!")
-        attempt += 1        
-        continue        
-        
+        attempt += 1
+        continue
+
     if x < number:
         print("Oops! It's higher. Please try again!")
         attempt += 1
-        continue 
+        continue
 
-    else:     
+    else:
         print("Congratulations! You are correct!")
         attempt += 1
         print("You have tried {} times.".format(attempt))
@@ -74,15 +45,8 @@ while True:
             attempt = 0
             import random
             number = random.randint(0,10)
-            print("The High Score is {}".format(min(score_list))) 
-            continue           
+            print("The High Score is {}".format(min(score_list)))
+            continue
         else:
-            print("Endgame. Thank you for playing!")         
+            print("Endgame. Thank you for playing!")
             break
-    
-
-
-
-
-
-
